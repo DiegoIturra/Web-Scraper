e73c0a0d485fca680c0915445ea2e9119e4e0990
Diego Iturra <diturra@dparadig.com>
2021-07-30 23:17:07 -0400

    add a text Parser method

--- scraper.py
+++ scraper.py
@@ -7,14 +7,20 @@ class Scraper:
 	
 	#Get name for every book
 	def __getBookName(self,soup):
-		return [divtag.text for divtag in soup.find_all('div',{'class':'h4'})]
+		names = soup.find_all('div',{'class':'h4'})
+		bookNames = []
+		for bookName in names:
+			bookName = Utils.translateText(bookName.text.strip())
+			bookNames.append(bookName)
+		return bookNames
 
+		
 	#Get original price for every book
-	def __getBookPrice(self,soup):
+	def __getBookPrices(self,soup):
 		prices = []
 		for divtag in soup.find_all('div',{'class':'price__regular'}):
 			for spanTag in divtag.find('span',{'class':'price-item price-item--regular'}):
-				prices.append(spanTag)
+				prices.append(spanTag.strip())
 		return prices
 
 
@@ -27,6 +33,11 @@ class Scraper:
 	#Get the link to the book data
 	def __getBookPath(self,soup):
 		return [path['href'] for path in soup.find_all('a',{'class':'grid-view-item__link grid-view-item__image-container full-width-link'})]
+		
+
+	#Get the image path from each book
+	def __getImagePath(self,soup):
+		pass
 
 
 	#Get data from all pages
@@ -36,16 +47,36 @@ class Scraper:
 
 		numPages = self.__getTotalPages(soup)
 		bookNames = []
+		bookPrices = []
 		for page in range(1,numPages+1):
 			url = self.URL + str(page)
 			request = requests.get(url)
 			soup = BeautifulSoup(request.text,'html.parser')
+			
 			currentBookNames = self.__getBookName(soup)
+			currentBookPrices = self.__getBookPrices(soup)
 
-			for current in currentBookNames:
-				bookNames.append(current)
+			for currentName in currentBookNames:
+				bookNames.append(currentName)
+			
+			for currentPrice in currentBookPrices:
+				bookPrices.append(currentPrice)
+		
+		dictionary = dict(zip(bookNames,bookPrices))
+		return dictionary
 
 
+class Utils:
 
-		return "Success!"
+	@staticmethod
+	def translateText(text):
+		""" method to translate latin characters """
+		charsToTranslate = "ÁÉÍÓÚáéíóoúñ"
+		objectiveText = "AEIOUaeioun"
+		try:
+			translation = str.maketrans(charsToTranslate,objectiveText)
+			return text.translate(translation)
+		except:
+			return text
+		
 
