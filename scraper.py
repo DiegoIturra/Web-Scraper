4c67b191f0964efd227ae69d5d8a6fea4695c15c
Diego Iturra <diturra@dparadig.com>
2021-08-08 20:55:17 -0400

    return type add to metohds

--- scraper.py
+++ scraper.py
@@ -1,92 +1,85 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+
 
 class Scraper:
+    URL = "https://literaturalibertad.com/collections/all?page="
+    mainURL = "https://literaturalibertad.com"
 
-	URL = "https://literaturalibertad.com/collections/all?page="
-	mainURL = "https://literaturalibertad.com"
-	
-	#Get name for every book
-	def __getBookName(self,soup):
-		names = soup.find_all('div',{'class':'h4'})
-		bookNames = []
-		for bookName in names:
-			bookName = Utils.translateText(bookName.text.strip())
-			bookNames.append(bookName)
-		return bookNames
+    # Get name for every book
+    def __get_book_names(self, soup: BeautifulSoup) -> list[str]:
+        names = soup.find_all('div', {'class': 'h4'})
+        bookNames = []
+        for bookName in names:
+            bookName = Utils.translate_text(bookName.text.strip())
+            bookNames.append(bookName)
+        return bookNames
 
-		
-	#Get original price for every book
-	def __getBookPrices(self,soup):
-		prices = []
-		for divtag in soup.find_all('div',{'class':'price__regular'}):
-			for spanTag in divtag.find('span',{'class':'price-item price-item--regular'}):
-				prices.append(spanTag.strip())
-		return prices
+    # Get original price for every book
+    def __getBookPrices(self, soup: BeautifulSoup) -> list[str]:
+        prices = []
+        for divtag in soup.find_all('div', {'class': 'price__regular'}):
+            for spanTag in divtag.find('span', {'class': 'price-item price-item--regular'}):
+                prices.append(spanTag.strip())
+        return prices
 
+    # Get count of total pages for scraping
+    def __getTotalPages(self, soup: BeautifulSoup) -> int:
+        textPages = soup.find('li', {'class': 'pagination__text'}).text
+        textList = textPages.strip().split(' ')
+        return int(textList[len(textList) - 1])
 
-	#Get count of total pages for scraping
-	def __getTotalPages(self,soup):
-		textPages = soup.find('li',{'class':'pagination__text'}).text
-		textList = textPages.strip().split(' ')
-		return int(textList[len(textList)-1])
+    # Get the link to the book data
+    def __getBookLink(self, soup: BeautifulSoup) -> list[str]:
+        return [path['href'] for path in
+                soup.find_all('a', {'class': 'grid-view-item__link grid-view-item__image-container full-width-link'})]
 
-	#Get the link to the book data
-	def __getBookLink(self,soup):
-		return [path['href'] for path in soup.find_all('a',{'class':'grid-view-item__link grid-view-item__image-container full-width-link'})]
-		
-	
-	#Get data from all pages
-	def scrapingData(self):
-		request = requests.get(self.URL)
-		soup = BeautifulSoup(request.text,'html.parser')
+    # Get data from all pages
+    def scrapingData(self) -> list[dict]:
+        request = requests.get(self.URL)
+        soup = BeautifulSoup(request.text, 'html.parser')
 
-		numPages = self.__getTotalPages(soup)
-		bookTitles = []
-		bookPrices = []
-		bookLinks = []
-		for page in range(1,numPages+1):
-			url = self.URL + str(page)
-			request = requests.get(url)
-			soup = BeautifulSoup(request.text,'html.parser')
-			
-			currentBookNames = self.__getBookName(soup)
-			currentBookPrices = self.__getBookPrices(soup)
-			currentBookLinks = self.__getBookLink(soup)
+        num_pages = self.__getTotalPages(soup)
+        book_titles = []
+        book_prices = []
+        book_links = []
+        for page in range(1, num_pages + 1):
+            url = self.URL + str(page)
+            request = requests.get(url)
+            soup = BeautifulSoup(request.text, 'html.parser')
 
-			#get title of books
-			for currentName in currentBookNames:
-				bookTitles.append(currentName)
-			
-			#get prices of books
-			for currentPrice in currentBookPrices:
-				bookPrices.append(currentPrice)
+            currentBookNames = self.__get_book_names(soup)
+            currentBookPrices = self.__getBookPrices(soup)
+            currentBookLinks = self.__getBookLink(soup)
 
-			#get links of books
-			for currentLink in currentBookLinks:
-				bookLinks.append(self.mainURL + currentLink)
+            # get title of books
+            for currentName in currentBookNames:
+                book_titles.append(currentName)
 
-		#list of dictionaries
-		data = [{'title':title, 'price': price, 'link': link} for title, price, link in zip(bookTitles, bookPrices, bookLinks)]
-		
-		return data
+            # get prices of books
+            for currentPrice in currentBookPrices:
+                book_prices.append(currentPrice)
 
+            # get links of books
+            for currentLink in currentBookLinks:
+                book_links.append(self.mainURL + currentLink)
 
+        # list of dictionaries
+        data = [{'title': title, 'price': price, 'link': link} for title, price, link in
+                zip(book_titles, book_prices, book_links)]
+
+        return data
 
 
 class Utils:
 
-	@staticmethod
-	def translateText(text):
-		""" method to translate latin characters """
-		charsToTranslate = "ÁÉÍÓÚáéíóúñ"
-		objectiveText = "AEIOUaeioun"
-		try:
-			translation = str.maketrans(charsToTranslate,objectiveText)
-			return text.translate(translation)
-		except:
-			return text
-		
-
-
+    @staticmethod
+    def translate_text(text) -> str:
+        """ method to translate latin characters """
+        chars_to_translate = "ÁÉÍÓÚáéíóúñ"
+        objective_text = "AEIOUaeioun"
+        try:
+            translation = str.maketrans(chars_to_translate, objective_text)
+            return text.translate(translation)
+        except:
+            return text
